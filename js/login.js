c2a2560ffc73e3b2c2a51b0b74de33507cf5a165
SanjaySasimandir <18615926+SanjaySasimandir@users.noreply.github.com>
2020-11-18 12:48:05 +0530

    new

--- js/login.js
+++ js/login.js
@@ -7,9 +7,8 @@ function loginValidation() {
     let regexp = /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3}?)$/;
     var flag = 0;
     if (regexp.test(inputEmail.value) == false) {
-        inputEmail.style.border = "2px solid red";
+        inputEmail.style.border = "2px solid #DC3545";
         emailError.innerText = "Invalid Email";
-        emailError.style.color = "red";
         flag = 1;
     } else {
         if (emailError.innerText == "Invalid Email") {
@@ -19,7 +18,7 @@ function loginValidation() {
     }
     regexp = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
     if (regexp.test(inputPassword.value) == false) {
-        inputPassword.style.border = "2px solid red";
+        inputPassword.style.border = "2px solid #DC3545";
         passwordError.innerText = "Invalid Password";
         passwordError.style.color = "red";
         flag = 1;
@@ -36,4 +35,49 @@ function loginValidation() {
         return true;
     }
 
+}
+function passwordStrength() {
+    var p = inputPassword.value;
+    var errors = [];
+    if (p.length < 8) {
+        errors.push("Your password will be at least 8 characters");
+    }
+    if (p.search(/[a-z]/) < 0) {
+        errors.push("Your password will contain at least one lowercase letter.");
+    }
+    if (p.search(/[A-Z]/) < 0) {
+        errors.push("Your password will contain at least one uppercase letter.");
+    }
+    if (p.search(/[0-9]/) < 0) {
+        errors.push("Your password will contain at least one number.");
+    }
+    if (errors.length > 0) {
+        passwordError.innerText = (errors.join("\n"));
+        passwordError.style.fontSize = "12px";
+        passwordError.removeAttribute("hidden", true);
+        inputPassword.style.border = "2px solid #DC3545";
+        return false;
+    }
+    else {
+        passwordError.innerText = "";
+        passwordError.setAttribute("hidden", true);
+        inputPassword.style.border = "";
+    }
+
+    return true;
+}
+function emailValidate() {
+    let regexp = /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3}?)$/;
+    if (regexp.test(inputEmail.value) == false) {
+        inputEmail.style.border = "2px solid #DC3545";
+        emailError.innerText = "Invalid Email";
+        emailError.style.fontSize = "12px";
+        emailError.removeAttribute("hidden", "true");
+    } else {
+        if (emailError.innerText == "Invalid Email") {
+            emailError.innerText = "";
+            inputEmail.style.border = "";
+            emailError.setAttribute("hidden", "true");
+        }
+    }
 }
